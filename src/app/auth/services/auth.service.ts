30f18b9a7a90c4024700d0406b57f6571fa4ec2a
AhmedYasser-js <ahmedkasso22@gmail.com>
2024-04-01 00:39:09 +0200

    integration API

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private _HttpClient: HttpClient) { }
+
+  onLogin(data: object): Observable<any> {
+    return this._HttpClient.post('/admin/users/login', data)
+  }
+
+}

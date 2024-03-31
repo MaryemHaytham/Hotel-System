ecd0cc3b6d32fec93718d2944d55ad69c2ff8853
MaryemHaytham <maryemhaythamm@gmail.com>
2024-03-31 18:31:14 +0200

    interceptor is done

--- src/app/core/interceptor/global.interceptor.ts
+++ src/app/core/interceptor/global.interceptor.ts
@@ -13,6 +13,25 @@ export class GlobalInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+
+    const token = localStorage.getItem('token');
+    const baseUrl: string = 'https://154.41.228.234:3000/api/v0'
+
+
+
+    let newHeaders = {};
+    
+    if (token !== null) {
+
+      newHeaders = {
+        'Authorization': `Bearer ${token}`
+      }
+    }
+    let cloned = request.clone({
+      setHeaders: newHeaders,
+      url: baseUrl + request.url
+    })
+
+    return next.handle(cloned);
   }
 }

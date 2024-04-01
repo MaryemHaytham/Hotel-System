6b00b24213e247a1d045473585a15c9c7223c3a0
AhmedYasser-js <ahmedkasso22@gmail.com>
2024-04-02 00:53:01 +0200

    guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageModule } from './landing-page/landing-page.module';
+import { adminGuard } from './core/guards/admin/admin.guard';
 
 const routes: Routes = [
-  {path:"", redirectTo:"auth",pathMatch:"full"},
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, 
-  { path: 'landing-page', loadChildren: () => import('./landing-page/landing-page.module').then(m => m.LandingPageModule) }, 
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }];
+  { path: "", redirectTo: "auth", pathMatch: "full" },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'landing-page', loadChildren: () => import('./landing-page/landing-page.module').then(m => m.LandingPageModule) },
+  { path: 'admin', canActivate: [adminGuard], loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -17,27 +17,25 @@ export class LoginComponent {
   see: boolean = true;
 
 
-  constructor(private _AuthService: AuthService,private _Router:Router) { }
+  constructor(private _AuthService: AuthService, private _Router: Router) { }
 
   loginForm: FormGroup = new FormGroup({
     email: new FormControl(null, [Validators.required, Validators.email]),
-    password: new FormControl(null, [Validators.required,  Validators.maxLength(20), Validators.minLength(8)])
+    password: new FormControl(null, [Validators.required, Validators.maxLength(20), Validators.minLength(8)])
   })
-  
+
 
   handleForm(data: FormGroup): void {
-    
-    
+
+
     let userData = data.value;
     this._AuthService.onLogin(userData).subscribe({
       next: (response) => {
+        localStorage.setItem('userToken', response.data.token);
       }, error: (error) => {
-        
+        console.log(error)
       }, complete: () => {
         this._Router.navigate(["admin"])
-        
-        
-
       }
     })
   }

e163f65fea621470f279caa56dd8dabfc6e675d1
MaryemHaytham <maryemhaythamm@gmail.com>
2024-04-01 23:21:27 +0200

    edits

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent },
+const routes: Routes = [{ path: '', component: AdminComponent ,children:[{ path: '', redirectTo:'home',pathMatch:'full'},
 { path: 'users', loadChildren: () => import('./Modules/users/users.module').then(m => m.UsersModule) },
 { path: 'rooms', loadChildren: () => import('./Modules/rooms/rooms.module').then(m => m.RoomsModule) },
 { path: 'ads', loadChildren: () => import('./Modules/ads/ads.module').then(m => m.AdsModule) },
@@ -10,7 +10,8 @@ const routes: Routes = [{ path: '', component: AdminComponent },
 { path: 'facilities', loadChildren: () => import('./Modules/facilities/facilities.module').then(m => m.FacilitiesModule) },
 { path: 'home', loadChildren: () => import('./Modules/home/home.module').then(m => m.HomeModule) },
 { path: 'comments', loadChildren: () => import('./Modules/comments/comments.module').then(m => m.CommentsModule) },
-{ path: 'favorites', loadChildren: () => import('./Modules/favorites/favorites.module').then(m => m.FavoritesModule) }, 
+{ path: 'favorites', loadChildren: () => import('./Modules/favorites/favorites.module').then(m => m.FavoritesModule) }]},
+
 ];
 
 @NgModule({
